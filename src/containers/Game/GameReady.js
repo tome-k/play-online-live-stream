dbd9f70dda21b73ed94b8b327c049e6411c116a7
nicholas-bd <nicholas.bond.d@gmail.com>
2019-10-05 15:27:48 +0300

    text loading issue with pixi js

--- src/containers/Game/GameReady.js
+++ src/containers/Game/GameReady.js
@@ -1,98 +1,38 @@
-import * as ExpoPixi from 'expo-pixi';
-import React, { Component } from 'react';
-import { Button, Platform, AppState, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { GLView } from 'expo-gl';
+import ExpoPixi,{ PIXI } from 'expo-pixi';
 
-const isAndroid = Platform.OS === 'android';
-function uuidv4() {
-    //https://stackoverflow.com/a/2117523/4047926
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (Math.random() * 16) | 0,
-            v = c == 'x' ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-    });
+export default class GameReady extends React.Component {
+  render() {
+    return (
+      <GLView
+        style={{ flex: 1 }}
+        onContextCreate={async context => {
+          const sprite = await PIXI.Sprite.fromExpoAsync(require('../../assets/images/icon.png'));
+          const app = new PIXI.Application({ context, backgroundColor: 0xffffff });
+          app.stage.addChild(sprite);
+          const style = new PIXI.TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 36,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#ffffff', '#00ff99'], // gradient
+            stroke: '#4a1850',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+          });
+          const basicText = new ExpoPixi.Text('Basic text in pixi', style);
+          basicText.x = 50;
+          basicText.y = 100;
+          app.stage.addChild(basicText);
+        }}
+      />
+    );
+  }
 }
-
-export default class GameReady extends Component {
-    state = {
-        image: null,
-        strokeColor: 0,
-        appState: AppState.currentState,
-    };
-
-    handleAppStateChangeAsync = nextAppState => {
-        if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
-            if (isAndroid && this.sketch) {
-                this.setState({ appState: nextAppState, id: uuidv4(), lines: this.sketch.lines });
-                return;
-            }
-        }
-        this.setState({ appState: nextAppState });
-    };
-
-    componentDidMount() {
-        AppState.addEventListener('change', this.handleAppStateChangeAsync);
-    }
-
-    componentWillUnmount() {
-        AppState.removeEventListener('change', this.handleAppStateChangeAsync);
-    }
-
-    onReady = () => {
-        console.log('ready!');
-    };
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.container}>
-                    <View style={styles.sketchContainer}>
-                        <ExpoPixi.Signature
-                            ref={ref => (this.sketch = ref)}
-                            style={styles.sketch}
-                            strokeColor={'blue'}
-                            strokeAlpha={1}
-                            onReady={this.onReady}
-                        />
-                    </View>
-                </View>
-                <Button
-                    color={'blue'}
-                    title="undo"
-                    style={styles.button}
-                    onPress={() => {
-                        this.sketch.undo();
-                    }}
-                />
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    sketch: {
-        flex: 1,
-    },
-    sketchContainer: {
-        height: '100%',
-    },
-    image: {
-        flex: 1,
-    },
-    label: {
-        width: '100%',
-        padding: 5,
-        alignItems: 'center',
-    },
-    button: {
-        // position: 'absolute',
-        // bottom: 8,
-        // left: 8,
-        zIndex: 1,
-        padding: 12,
-        minWidth: 56,
-        minHeight: 48,
-    },
-});
