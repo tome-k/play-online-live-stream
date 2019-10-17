1afbeaa70f2c8d2cc6b6a75aceac1436569a9f98
nicholas-bd <nicholas.bond.d@gmail.com>
2019-10-17 18:17:42 +0300

    game play time update

--- src/containers/Game/gameEngine/index.js
+++ src/containers/Game/gameEngine/index.js
@@ -36,7 +36,7 @@ export default function GamePlay({ backPage }) {
   const [score, setScore] = React.useState(0);
   const [passPlayers, setpassPlayers] = React.useState(0);
   const [bulletCount, setbulletCount] = React.useState(100);
-  const [gamePlayTime, setGamePlayTime] = React.useState(15);
+  const [gamePlayTime, setGamePlayTime] = React.useState(100);
   const [gameHitData, setGameHitData] = React.useState({});
   const [gameStartInternal, setGameStartInternal] = React.useState(null);
   const [shotSoundObjectSingle, setShotSoundObjectSingle] = useVariable(null);
